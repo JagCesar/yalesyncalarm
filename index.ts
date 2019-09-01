3158d6f81ce772b17328e7a2a29331c0c15a1685
Jonathan Dann <jonathandann@users.noreply.github.com>
2019-09-01 22:20:43 +0100

    eslint and prettier configs

--- index.ts
+++ index.ts
@@ -1,87 +1,87 @@
-import * as NodeFetch from "node-fetch";
+import * as NodeFetch from 'node-fetch'
 
 const yaleAuthToken =
-  "VnVWWDZYVjlXSUNzVHJhcUVpdVNCUHBwZ3ZPakxUeXNsRU1LUHBjdTpkd3RPbE15WEtENUJ5ZW1GWHV0am55eGhrc0U3V0ZFY2p0dFcyOXRaSWNuWHlSWHFsWVBEZ1BSZE1xczF4R3VwVTlxa1o4UE5ubGlQanY5Z2hBZFFtMHpsM0h4V3dlS0ZBcGZzakpMcW1GMm1HR1lXRlpad01MRkw3MGR0bmNndQ==";
+	'VnVWWDZYVjlXSUNzVHJhcUVpdVNCUHBwZ3ZPakxUeXNsRU1LUHBjdTpkd3RPbE15WEtENUJ5ZW1GWHV0am55eGhrc0U3V0ZFY2p0dFcyOXRaSWNuWHlSWHFsWVBEZ1BSZE1xczF4R3VwVTlxa1o4UE5ubGlQanY5Z2hBZFFtMHpsM0h4V3dlS0ZBcGZzakpMcW1GMm1HR1lXRlpad01MRkw3MGR0bmNndQ=='
 
 const enum Path {
-  auth = "o/token",
-  panelMode = "api/panel/mode",
-  deviceStatus = "api/panel/device_status"
+	auth = 'o/token',
+	panelMode = 'api/panel/mode',
+	deviceStatus = 'api/panel/device_status',
 }
 
 function url(path: Path): string {
-  return `https://mob.yalehomesystem.co.uk/yapi/${path}/`;
+	return `https://mob.yalehomesystem.co.uk/yapi/${path}/`
 }
 
 function headersWithAccessToken(accessToken: string): Headers {
-  return new Headers({
-    Authorization: `Bearer ${accessToken}`
-  });
+	return new Headers({
+		Authorization: `Bearer ${accessToken}`,
+	})
 }
 
 export async function getAccessToken(
-  username: string,
-  password: string
+	username: string,
+	password: string
 ): Promise<string> {
-  let payload = `grant_type=password&username=${encodeURIComponent(
-    username
-  )}&password=${encodeURIComponent(password)}`;
+	let payload = `grant_type=password&username=${encodeURIComponent(
+		username
+	)}&password=${encodeURIComponent(password)}`
 
-  let response = await fetch(url(Path.auth), {
-    method: "POST",
-    body: payload,
-    headers: {
-      Authorization: `Basic ${yaleAuthToken}`
-    }
-  });
-  let json = await response.json();
-  if (json.error === "invalid_grant") {
-    throw new Error(json.error_description);
-  } else {
-    return json.access_token;
-  }
+	let response = await fetch(url(Path.auth), {
+		method: 'POST',
+		body: payload,
+		headers: {
+			Authorization: `Basic ${yaleAuthToken}`,
+		},
+	})
+	let json = await response.json()
+	if (json.error === 'invalid_grant') {
+		throw new Error(json.error_description)
+	} else {
+		return json.access_token
+	}
 }
 
 export const enum AlarmState {
-  arm = "arm",
-  home = "home",
-  disarm = "disarm"
+	arm = 'arm',
+	home = 'home',
+	disarm = 'disarm',
 }
 
 export async function setStatus(
-  accessToken: string,
-  alarmState: AlarmState
+	accessToken: string,
+	alarmState: AlarmState
 ): Promise<string> {
-  if (!accessToken || accessToken.length === 0) {
-    throw new Error(
-      "Please call getAccessToken to get your access token first."
-    );
-  }
+	if (!accessToken || accessToken.length === 0) {
+		throw new Error(
+			'Please call getAccessToken to get your access token first.'
+		)
+	}
 
-  let response = await fetch(url(Path.panelMode), {
-    method: "POST",
-    body: `area=1&mode=${alarmState}`,
-    headers: headersWithAccessToken(accessToken)
-  });
-  let json = await response.json();
-  let setStatus = json.data.cmd_ack;
-  return setStatus;
+	let response = await fetch(url(Path.panelMode), {
+		method: 'POST',
+		body: `area=1&mode=${alarmState}`,
+		headers: headersWithAccessToken(accessToken),
+	})
+	let json = await response.json()
+	let setStatus = json.data.cmd_ack
+	return setStatus
 }
 
 export async function getStatus(accessToken: string): Promise<AlarmState> {
-  if (!accessToken || accessToken.length === 0) {
-    throw new Error(
-      "Please call getAccessToken to get your access token first."
-    );
-  }
+	if (!accessToken || accessToken.length === 0) {
+		throw new Error(
+			'Please call getAccessToken to get your access token first.'
+		)
+	}
 
-  let response = await fetch(url(Path.panelMode), {
-    method: "GET",
-    headers: headersWithAccessToken(accessToken)
-  });
-  let json = await response.json();
-  let alarmState = json.data[0].mode;
-  return alarmState;
+	let response = await fetch(url(Path.panelMode), {
+		method: 'GET',
+		headers: headersWithAccessToken(accessToken),
+	})
+	let json = await response.json()
+	let alarmState = json.data[0].mode
+	return alarmState
 }
 
 // export function getDevices(access_token: string): Promise<any> {
